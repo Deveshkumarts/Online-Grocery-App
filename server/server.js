884608fb04c1b8492736e9b0b39dd16ebe9187c1
Devesh Kumar <deveshk17122006@gmail.com>
2026-05-17 11:40:55 +0530

    Fix: Configure CORS for deployment

--- server/server.js
+++ server/server.js
@@ -13,7 +13,17 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3000'].filter(Boolean);
+app.use(cors({ 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, 
+  credentials: true 
+}));
 app.use(express.json());
 app.use(cookieParser());
 
